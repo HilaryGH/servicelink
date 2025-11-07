7cf2f8b27ef9f55725bc5024c785dfe1b7a9ca68
Hilari Gebremedhin <hilarygebremedhn28@gmail.com>
2025-11-07 00:23:47 -0800

    Build: add dist folder for deployment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+   base: "./",     // ✅ Add this
   plugins: [react(),tailwindcss(),
   ]
 })
